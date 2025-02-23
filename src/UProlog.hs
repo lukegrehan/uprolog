76db8097072816b70b2ab607768fd9a3daf14b5f
Luke Grehan <luketenant@gmail.com>
2025-02-23 16:49:48 +0000

    .

--- src/UProlog.hs
+++ src/UProlog.hs
@@ -10,3 +10,12 @@ import Prelude hiding (any, all, and, or)
 import UProlog.Val
 import UProlog.State
 import UProlog.Util
+
+appendP' :: (LogicVal a, LogicVal b, LogicVal c) => a -> b -> c -> Pred
+appendP' a b r = appendP (quote a) (quote b) (quote r)
+
+test = runVals $ \v -> withN 3 $ \[a,b,c] -> (P (a,P (b,c)) === v) `and` with (\t -> (appendP' a b t) `and` (appendP' t c [1::Integer,2,3,4,5,6,7,8,9]))
+
+pprint :: Val -> String
+pprint (P (a,P (b,c))) = show a ++ "\t\t" ++ show b ++ "\t\t" ++ show c
+pprint _ = "<something else>"
